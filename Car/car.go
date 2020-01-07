3b602aaa487c864753b655ef8b7cef0e21a6ca4d
BEOMJUN KIM <skyblue300a@gmail.com>
2020-01-07 15:32:18 +0900

    feat: 자동차 전진 기능 구현

--- Car/car.go
+++ Car/car.go
@@ -1,6 +1,11 @@
 package Car
 
+import "../Util"
+
 const initialPosition = 0
+const minRandomValue = 0
+const maxRandomValue = 9
+const thresholdMoveForward = 4
 
 var cars []*Car
 
@@ -13,14 +18,34 @@ type Car struct {
 	position int
 }
 
-func NewCars(carsName[] string) {
+func NewCars(carsName [] string) {
 	for _, carName := range carsName {
-		cars = append(cars,
-			newCar(carName))
+		cars = append(cars, newCar(carName))
 	}
+}
+
+func CarsMoveForward() []*Car {
+	for _, car := range cars {
+		car.moveForward()
+	}
+	return cars
 }
 
 func newCar(name string) *Car {
 	newCar := Car{name, initialPosition}
 	return &newCar
+}
+
+func (car Car) moveForward() {
+	if canMoveForward() {
+		car.position++
+	}
+}
+
+func canMoveForward() bool {
+	randomNumber := Util.MakeRandomInt(minRandomValue, maxRandomValue)
+	if randomNumber < thresholdMoveForward {
+		return false
+	}
+	return true
 }
---
+++ Util/util.go
@@ -0,0 +1,7 @@
+package Util
+
+import "math/rand"
+
+func MakeRandomInt(min int, max int) int {
+	return rand.Intn(max - min + 1) + min
+}
